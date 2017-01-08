01ec12bf186a195612e70f85183af47f83d502aa
rtfuzz <rtfuzz@gmail.com>
2017-01-08 13:36:23 -0200

    app

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={	
+	'ApiUrl' : "http://www.apetitego.com.br/merchantapp/api",
+	'DialogDefaultTitle' : "ApetiteGO",
+	'pushNotificationSenderid' : "1658883061078753",
+	'facebookAppId' : "172997422108",
+	'APIHasKey' : "@La2483801317"
+};
